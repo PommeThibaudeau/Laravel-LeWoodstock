929126835264898425cc38d47b5f76805ae6355e
pierre.T <pierre.thibaudeau3496@gmail.com>
2018-06-21 18:43:56 +0200

    correct spelling mistake on type 2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception type that are not reported.
+     * A list of the exception types that are not reported.
      *
      * @var array
      */
