c91881563a404de193eb6b2db1a71b7715fc30f4
Pierre Thibaudeau <pierre.thibaudeau3496@gmail.com>
2018-07-03 17:02:55 +0200

    mobile parralax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,6 @@
     <!-- Chosen -->
     <script src="{{ asset('js/chosen/chosen.jquery.min.js') }}" defer></script>
 
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
